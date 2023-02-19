917dd2f030c37d73833ab14ce675529709c68ada
ConstDet <constx7@gmail.com>
2023-02-19 17:00:06 +0300

    Закончил катать народ.

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,58 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
 public class Main {
     protected static List<Person> generateClient() {
-        Person person = new Person();
         List<Person> list = new ArrayList<>();
-        person.name = "Petya";
-        person.lastName = "Smirnoff";
-        person.numberTickets = 5;
-        list.add(person);
+        Person person0 = new Person();
+        person0.name = "Petya";
+        person0.lastName = "Smirnoff";
+        person0.numberTickets = 5;
+        list.add(person0);
         //--------------------------------
-        person.name = "Masha";
-        person.lastName = "Mishkina";
-        person.numberTickets = 3;
-        list.add(person);
+        Person person1 = new Person();
+        person1.name = "Masha";
+        person1.lastName = "Mishkina";
+        person1.numberTickets = 3;
+        list.add(person1);
         //--------------------------------
-        person.name = "Dima";
-        person.lastName = "Petrov";
-        person.numberTickets = 1;
-        list.add(person);
+        Person person2 = new Person();
+        person2.name = "Dima";
+        person2.lastName = "Petrov";
+        person2.numberTickets = 1;
+        list.add(person2);
         //--------------------------------
-        person.name = "Katya";
-        person.lastName = "Bulkina";
-        person.numberTickets = 4;
-        list.add(person);
+        Person person3 = new Person();
+        person3.name = "Katya";
+        person3.lastName = "Bulkina";
+        person3.numberTickets = 4;
+        list.add(person3);
         //--------------------------------
-        person.name = "Dunya";
-        person.lastName = "Burlakova";
-        person.numberTickets = 2;
-        list.add(person);
+        Person person4 = new Person();
+        person4.name = "Dunya";
+        person4.lastName = "Burlakova";
+        person4.numberTickets = 2;
+        list.add(person4);
         //--------------------------------
         return list;
     }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Deque<Person> dequePerson = new ArrayDeque<>();
+
+        for (int i = 0; i < generateClient().size(); i++) {
+            dequePerson.offer(generateClient().get(i));
+        }
+
+        while (!dequePerson.isEmpty()) {
+            Person person = dequePerson.pollFirst();//вытащим первого в очереди
+            if (person.numberTickets > 1) {//два и более билета, то прокатим еще раз и в очередь
+                person.numberTickets -= 1;
+                dequePerson.offerLast(person);//обратно в очередь
+            } else if (person.numberTickets == 1) {//прокатим еще раз и удалим из очереди
+                person.numberTickets -= 1;
+            }
+            System.out.println(person.name + " " + person.lastName +
+                    " прокатился на аттракционе! Осталось билетов - " + person.numberTickets);
+        }
+        System.out.println("Всех покатали, очередь закончилась!");
     }
 }
