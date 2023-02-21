5860c2657688a6adff0b6aba5e8e20dc76978f66
ConstDet <constx7@gmail.com>
2023-02-21 20:42:16 +0300

    Доработал класс Person

--- src/Main.java
+++ src/Main.java
@@ -3,36 +3,16 @@ import java.util.*;
 public class Main {
     protected static List<Person> generateClient() {
         List<Person> list = new ArrayList<>();
-        Person person0 = new Person();
-        person0.name = "Petya";
-        person0.lastName = "Smirnoff";
-        person0.numberTickets = 5;
+        Person person0 = new Person("Petya", "Smirnoff", 5);
         list.add(person0);
-        //--------------------------------
-        Person person1 = new Person();
-        person1.name = "Masha";
-        person1.lastName = "Mishkina";
-        person1.numberTickets = 3;
+        Person person1 = new Person("Masha", "Mishkina", 3);
         list.add(person1);
-        //--------------------------------
-        Person person2 = new Person();
-        person2.name = "Dima";
-        person2.lastName = "Petrov";
-        person2.numberTickets = 1;
+        Person person2 = new Person("Dima", "Petrov", 1);
         list.add(person2);
-        //--------------------------------
-        Person person3 = new Person();
-        person3.name = "Katya";
-        person3.lastName = "Bulkina";
-        person3.numberTickets = 4;
+        Person person3 = new Person("Katya", "Bulkina", 4);
         list.add(person3);
-        //--------------------------------
-        Person person4 = new Person();
-        person4.name = "Dunya";
-        person4.lastName = "Burlakova";
-        person4.numberTickets = 2;
+        Person person4 = new Person("Dunya", "Burlakova", 2);
         list.add(person4);
-        //--------------------------------
         return list;
     }
     public static void main(String[] args) {
@@ -44,14 +24,13 @@ public class Main {
 
         while (!dequePerson.isEmpty()) {
             Person person = dequePerson.pollFirst();//вытащим первого в очереди
-            if (person.numberTickets > 1) {//два и более билета, то прокатим еще раз и в очередь
-                person.numberTickets -= 1;
+            if (person.getNumberTickets() > 1) {//два и более билета, то прокатим еще раз и в очередь
+                person.consumptionTicket();//забрали один билет
                 dequePerson.offerLast(person);//обратно в очередь
-            } else if (person.numberTickets == 1) {//прокатим еще раз и удалим из очереди
-                person.numberTickets -= 1;
+            } else if (person.getNumberTickets() == 1) {//прокатим еще раз и удалим из очереди
+                person.consumptionTicket();//забрали один билет
             }
-            System.out.println(person.name + " " + person.lastName +
-                    " прокатился на аттракционе! Осталось билетов - " + person.numberTickets);
+            System.out.println(person.toString());
         }
         System.out.println("Всех покатали, очередь закончилась!");
     }
--- src/Person.java
+++ src/Person.java
@@ -1,5 +1,31 @@
 public class Person {
-    protected String name;
-    protected String lastName;
-    protected int numberTickets;
+    private String name;
+    private String lastName;
+    private int numberTickets;
+    public Person(String name, String lastName, int numberTickets) {
+        this.name = name;
+        this.lastName = lastName;
+        this.numberTickets = numberTickets;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getNumberTickets() {
+        return numberTickets;
+    }
+
+    public void consumptionTicket() {
+        numberTickets -= 1;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + lastName + " прокатился на аттракционе! Осталось билетов - " + numberTickets;
+    }
 }
