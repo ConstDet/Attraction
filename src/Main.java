f7272663de42e4393bd0f9a6555c8ca23e4f7c09
ConstDet <constx7@gmail.com>
2023-02-21 21:25:36 +0300

    Доработал метод generateClient(). Убрал цикл fori

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,17 @@ import java.util.*;
 public class Main {
     protected static List<Person> generateClient() {
         List<Person> list = new ArrayList<>();
-        Person person0 = new Person("Petya", "Smirnoff", 5);
-        list.add(person0);
-        Person person1 = new Person("Masha", "Mishkina", 3);
-        list.add(person1);
-        Person person2 = new Person("Dima", "Petrov", 1);
-        list.add(person2);
-        Person person3 = new Person("Katya", "Bulkina", 4);
-        list.add(person3);
-        Person person4 = new Person("Dunya", "Burlakova", 2);
-        list.add(person4);
+        list.add(new Person("Petya", "Smirnoff", 5));
+        list.add(new Person("Masha", "Mishkina", 3));
+        list.add(new Person("Dima", "Petrov", 1));
+        list.add(new Person("Katya", "Bulkina", 4));
+        list.add(new Person("Dunya", "Burlakova", 2));
         return list;
     }
     public static void main(String[] args) {
         Deque<Person> dequePerson = new ArrayDeque<>();
 
-        for (int i = 0; i < generateClient().size(); i++) {
-            dequePerson.offer(generateClient().get(i));
-        }
+        dequePerson.addAll(generateClient());
 
         while (!dequePerson.isEmpty()) {
             Person person = dequePerson.pollFirst();//вытащим первого в очереди
